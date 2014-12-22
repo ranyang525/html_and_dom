a5d27d25585ae890e3881998d34ea55b3bb64120
ranyang <ranyang525@gmail.com>
2014-12-22 16:00:01 +0800

    Refactor shortanswer.js

--- js/shortanswer.js
+++ js/shortanswer.js
@@ -7,10 +7,5 @@ ShortAnswer.prototype = Object.create(Question.prototype);
 ShortAnswer.prototype.constructor = ShortAnswer;
 
 ShortAnswer.prototype.getScore = function() {
-    var score = 0;
-    if (this.answer === document.getElementById(this.identifier).value) {
-      score = 20;
-    }
-    return score;
-
+    return this.answer === document.getElementById(this.identifier).value ? 20 :0;
 };
